d5d93df3a6856af3e0677b77f9afaac917686b67
ssg <ssg8697@naver.com>
2022-10-27 15:36:03 +0900

    test

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from flask import Flask
 from flask_cors import CORS
 import pymysql
 
-host = "192.168.42.202"
-user = "root"
-password = "qwer1234"
-db = "ssg_db"
-conn = pymysql.connect(host=host, user=user, password=password, charset='utf8')
-curs = conn.cursor()
-sql = "select * from student"
-curs.execute(sql)
-
-rows = curs.fatchall()
-conn.commit()
-conn.close()
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-
 @app.route('/hello')
 def hello():
-    result = {rows}
+    host = "192.168.42.202"
+    user = "root"
+    password = "qwer1234"
+    db = "ssg_db"
+    conn = pymysql.connect(host=host, user=user, password=password, charset='utf8')
+    curs = conn.cursor()
+    sql = "select * from student"
+    curs.execute(sql)
+
+    rows = curs.fatchall()
+    print(rows)
+    conn.commit()
+    conn.close()
+    result = {"code" : 200, "message":rows}
     return result
 
 if __name__ == "__main__":
